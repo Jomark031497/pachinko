389d85eb1f7a3e3fcf091adec0061a2ce1cdb743
Jomark Pangan <jomarkrazonpangan@gmail.com>
2025-06-12 17:37:21 +0800

    style: smaller font for transaction card

--- web/src/features/transactions/components/TransactionCard.tsx
+++ web/src/features/transactions/components/TransactionCard.tsx
@@ -12,35 +12,25 @@ interface Props {
 const TransactionCard = ({ transaction }: Props) => {
   const { user } = useAuth();
 
+  const isIncome = transaction.type === "income";
+
   return (
     <Link to={`/transactions/${transaction.id}`}>
       <div
         className={cn(
-          transaction.type === "income"
-            ? "bg-accent-green/5"
-            : transaction.type === "expense"
-              ? "bg-accent-red/5"
-              : "bg-yellow-400/10",
+          isIncome ? "bg-accent-green/5" : "bg-accent-red/5",
           "grid grid-cols-3 rounded border border-dashed border-gray-300 p-2 text-gray-500 shadow",
         )}
       >
-        <p className="col-span-2 text-sm font-semibold">{transaction.name}</p>
-        <p
-          className={cn(
-            transaction.type === "income"
-              ? "text-accent-green"
-              : transaction.type === "expense"
-                ? "text-accent-red"
-                : "text-yellow-400",
-            "text-right text-sm font-semibold",
-          )}
-        >
+        <p className="col-span-2 text-xs font-semibold">{transaction.name}</p>
+        <p className={cn(isIncome ? "text-accent-green" : "text-accent-red", "text-right text-xs font-semibold")}>
+          {isIncome ? "" : "-"}
           {toCurrency(transaction.amount, user?.currency)}
         </p>
-        <p className="col-span-2 text-sm italic">{transaction.category.name}</p>
-        <p className="text-right text-sm">{format(transaction.transactionDate, "MMM dd, yyyy")}</p>
-        <p className="col-span-2 text-sm">{transaction.account.name}</p>
-        <p className="text-right text-sm">{transaction.account.type}</p>
+        <p className="col-span-2 text-xs italic">{transaction.category.name}</p>
+        <p className="text-right text-xs">{format(transaction.transactionDate, "MMM dd, yyyy")}</p>
+        <p className="col-span-2 text-xs">{transaction.account.name}</p>
+        <p className="text-right text-xs capitalize">{transaction.account.type}</p>
       </div>
     </Link>
   );
