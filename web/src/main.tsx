9e2f05d5f5e370b1080f5e0f96cf214e0e335fd5
Jomark Pangan <jomarkrazonpangan@gmail.com>
2025-06-12 22:45:23 +0800

    style: removed fonts

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// @ts-expect-error TODO: fontsource issue with types
-import "@fontsource/fira-mono";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "~/lib/queryClient.ts";
@@ -12,7 +10,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
 );
